bec1f176b3a4b46df5e66eec7ca24650314c1e20
Marcos Vilela <marcos.vilela42@hotmail.com>
2019-11-28 09:56:36 -0300

    Add files via upload

---
+++ projetorna.r
@@ -0,0 +1,20 @@
+######## PROJETO REDES NEURAIS - ALDO MONTEIRO, ARYELL DIAS, MARCOS VILELA E NATÁLIA MAYER ###############
+
+######## PRÉ-PROCESSAMENTO DOS DATASETS ########
+
+######## ACHANDO OS PARÂMETROS ########
+
+####### EXPERIMENTOS PRINCIPAIS (SALVANDO OS RESULTADOS DAS MÉTRICAS (Acurácia e F1 Score) EM CSV) #######
+
+####### TESTES ESTATÍSTICOS #########
+
+#Shapiro-Wilk
+
+#Kolmogorov-Smirnov
+
+#Teste-F
+
+#t-Student
+
+#Wilcoxon
+
