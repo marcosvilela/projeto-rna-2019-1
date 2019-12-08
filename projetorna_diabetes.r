18307197c0e7c32449404c8d29f1fcc767d76aab
marcosvilela <marcos.vilela42@hotmail.com>
2019-12-08 16:07:11 -0300

    Finalizando pré-processamento de diabetes, primeira execução

--- projetorna_diabetes.r
+++ projetorna_diabetes.r
@@ -2,46 +2,71 @@
 
 library(zoo)
 library(neuralnet)
+library("mlbench")
 
 ######## PRÉ-PROCESSAMENTO DO DATASET ########
 
-diabetes <- read.csv('~/Documentos/code/redes-neurais/projeto-rna-2019-1/datasets/Pima Indians Diabetes Dataset.csv', header=FALSE)
-names(diabetes)<-c('n_pregnant','plasma_concentration','blood_pressure','skin_thickness','insulin','body_mass','diabetes_pedigree',
-                   'age','class')
-View(diabetes)
+data(PimaIndiansDiabetes) #Importando o dataset da  biblioteca mlbench
+diabetes<-PimaIndiansDiabetes
+#View(diabetes)
 diabetes_class<-diabetes$class #guardando as classes possíveis de diabetes
-diabetes<-diabetes[,1:8]
-print(summary(diabetes))
 
-#Diabetes -> Tratar missings "0" pela média não-nula da coluna
-#Os que não fazem sentido terem 0 são a primeira coluna e a última coluna. Todo o resto precisa ser substituido de alguma forma
-#Na segunda coluna, taxa de glicose, quando a classe final for Diabético, substituir o valor por 180. Quando não for, pela média da coluna
-diabetes$blood_pressure[diabetes$blood_pressure==0]<-NA
-diabetes$skin_thickness[diabetes$skin_thickness==0] <- NA
-diabetes$insulin[diabetes$insulin==0]<-NA
-diabetes$body_mass[diabetes$body_mass==0]<-NA
-diabetes$diabetes_pedigree[diabetes$diabetes_pedigree==0]<-NA
-diabetes$age[diabetes$age==0]<-NA
-finaldiabetes <- na.aggregate(diabetes) #Substitui os valores com missings pela média das colunas
-print(sum(is.na(finaldiabetes))) #Debug para ver a quantidade de missings restante
+#Mudamos as 5 ocorrencias de glucose=0 (que caracteriza um missing) para um valor-limite se a linha for de um diabético
+diabetes$glucose[350]<-180
+diabetes$glucose[503]<-180
+#Se não for, mudamos para a média da coluna
+diabetes$glucose[diabetes$glucose==0]<-as.numeric(mean(diabetes$glucose))
+#Todas as colunas seguintes podem ser substituídas pela média
+diabetes$pressure[diabetes$pressure==0]<-as.numeric(mean(diabetes$pressure))
+diabetes$triceps[diabetes$triceps==0] <- as.numeric(mean(diabetes$triceps))
+diabetes$insulin[diabetes$insulin==0]<-as.numeric(mean(diabetes$insulin))
+diabetes$mass[diabetes$mass==0]<-as.numeric(mean(diabetes$mass))
+diabetes$pedigree[diabetes$pedigree==0]<-as.numeric(mean(diabetes$pedigree))
+diabetes$age[diabetes$age==0]<-as.numeric(mode(diabetes$age)) #A idade é a que mais se repete
 
-#Transformando os dados em dataframes
-diabetes_dataframe <- as.data.frame(lapply(finaldiabetes, function(x) as.numeric(x)))
+print(sum(is.na(diabetes))) #Debug para ver a quantidade de missings restante
+finaldiabetes<-diabetes[,1:8]
+View(finaldiabetes)
+
+#Dividindo as classes de diabetes e não-diabetes
+Diabetes<-diabetes$diabetes
+Diabetes<-as.data.frame(Diabetes)
+Diabetes<-with(Diabetes,data.frame(model.matrix(~Diabetes+0)))
+#View(Diabetes)
+
+#Transformando os dados em dataframes 
+#diabetes_dataframe <- as.data.frame(diabetes)
+#View(diabetes_dataframe)
 
 #Normalizando os dados
-diabetes_max<-apply(diabetes_dataframe, 2, max)
-diabetes_min<-apply(diabetes_dataframe, 2, min)
-scaled_diabetes<-as.data.frame(scale(diabetes_dataframe, center=diabetes_min, scale=diabetes_max - diabetes_min))
+diabetes_max<-apply(finaldiabetes, 2, max)
+diabetes_min<-apply(finaldiabetes, 2, min)
+scaled_diabetes<-as.data.frame(scale(finaldiabetes, center=diabetes_min, scale=diabetes_max - diabetes_min))
+View(scaled_diabetes)
+
+final_data<-as.data.frame(cbind(scaled_diabetes, Diabetes))
+View(final_data)
 
 #Dividindo o datasets, 75% e 25%. O set.seed() é opcional, apenas para podermos checar o resultado depois
 #set.seed(200)
-train_index_diabetes<-sample(nrow(scaled_diabetes), 0.75*nrow(scaled_diabetes), replace=FALSE)
-diabetes_trainset<-scaled_diabetes[train_index_breast,]
-diabetes_testset<-scaled_diabetes[-train_index_breast,]
+train_index_diabetes<-sample(nrow(final_data), 0.75*nrow(final_data), replace=FALSE)
+diabetes_trainset<-as.data.frame(final_data[train_index_diabetes,])
+diabetes_testset<-as.data.frame(final_data[-train_index_diabetes,])
 
 View(diabetes_trainset)
+print(sum(is.na(diabetes_trainset)))
 
 ######## ACHANDO OS PARÂMETROS DO MODELO ########
+n<-names(final_data[1:8])
+f<-as.formula(paste("Diabetesneg + Diabetespos ~", paste(n, collapse='+')))
+
+net <- neuralnet(f, data=diabetes_trainset,hidden = 5,linear.output = FALSE)
+plot(net)
+predict_net_test <- compute(net,diabetes_testset[,1:8])
+predict_result<-round(predict_net_test$net.result, digits = 0)
+net.prediction = c("neg", "pos")[apply(predict_result, 1, which.max)]
+predict.table = table(diabetes$diabetes[-train_index_diabetes], net.prediction)
+print(predict.table)
 
 ####### EXPERIMENTOS PRINCIPAIS (SALVANDO OS RESULTADOS DAS MÉTRICAS (Acurácia e F1 Score) EM CSV) #######
 
